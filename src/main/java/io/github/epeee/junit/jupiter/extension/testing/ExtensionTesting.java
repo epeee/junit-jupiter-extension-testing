bc4bca90e891d93143325540eb95261aacc2c08a
epeee <epeee@users.noreply.github.com>
2018-07-08 22:05:19 +0200

    Use @CheckReturnValue

--- src/main/java/io/github/epeee/junit/jupiter/extension/testing/ExtensionTesting.java
+++ src/main/java/io/github/epeee/junit/jupiter/extension/testing/ExtensionTesting.java
@@ -1,9 +1,12 @@
 package io.github.epeee.junit.jupiter.extension.testing;
 
+import com.google.errorprone.annotations.CheckReturnValue;
+
 import static io.github.epeee.junit.jupiter.extension.testing.TestExecutionUtil.runTests;
 
 public class ExtensionTesting {
 
+    @CheckReturnValue
     public static TestResultAssert assertThatTest(Class<?> clazz) {
         return new TestResultAssert(runTests(clazz));
     }
